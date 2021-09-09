ddbb9d3183e950209355a7154f03726909bc6fd1
Arasi Arumugam <ezhil.arum@gmail.com>
2021-09-08 21:12:42 -0500

    Practice on Dynamic templates

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -13,30 +13,39 @@ import java.util.List;
 @Controller
 public class HelloController {
 
-    @GetMapping("goodbye")
-    @ResponseBody
-    public String goodbye() {
-        return "Goodbye, Spring!";
-    }
 
     // Handles requests of the form /hello?name=LaunchCode
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
-    @ResponseBody
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
+      public String helloWithQueryParam(@RequestParam String name, Model model) {
+        //return "Hello, " + name + "!";
+       String greeting = "Hello " + name + " !";
+       model.addAttribute("greeting", greeting);
+        return "hello";
     }
 
    // Handles requests of the form /hello/LaunchCode
     @GetMapping("hello/{name}")
-    @ResponseBody
-    public String helloWithPathParam(@PathVariable String name) {
-        return "Hello, " + name + "!";
+    public String helloWithPathParam(@PathVariable String name,Model model) {
+        String greeting = "Hello " + name + " !";
+        model.addAttribute("greeting",greeting);
+        return "hello";
     }
 
     // /hello/form
     @GetMapping("form")
     public String helloForm() {
         return "form";
+    }
+
+    @GetMapping("hello-names")
+    public String helloNames(Model model){
+        List<String> names = new ArrayList<>();
+        names.add("Launchcode");
+        names.add("Java");
+        names.add("Codergirl");
+        names.add("webdevelopment");
+        model.addAttribute("names",names);
+        return "helloList";
     }
 
 }
